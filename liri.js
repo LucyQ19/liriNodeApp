94baa9cb6b7e8e7f4fbe261a82d3efc981e9fa65
LucyQ19 <lucyq19@gmail.com>
2019-05-05 11:38:39 -0500

    fix spotify-this-song function

--- liri.js
+++ liri.js
@@ -1,15 +1,16 @@
 require("dotenv").config();
 
+const keys = require("./keys.js");
+
 const fs = require("fs");
 const axios = require("axios");
 const moment = require("moment");
 const Spotify = require("node-spotify-api");
 
-const keys = require("./keys.js");
 let spotify = new Spotify(keys.spotify);
 
 let command = process.argv[2];
-let search = process.argv[3];
+let searchTerm = process.argv[3];
 
 function storeAndDisplayData(formatted){
     const divider = "\n----------------------------------------------------------------------\n\n";
@@ -28,46 +29,46 @@ function storeAndDisplayData(formatted){
 switch(command){
 
     case "concert-this":
-        concertThis(search);
+        concertThis(searchTerm);
         break;
             
     case "spotify-this-song":
-        spotifyThis(search);
+        spotifyThis(searchTerm);
         break;
             
     case "movie-this":
-        movieThis(search);
+        movieThis(searchTerm);
         break;
                     
     case "do-what-it-says":
-        doThis(search);
+        doThis(searchTerm);
         break;
             
     default:
-        console.log("Please enter a command: concert-this, spotify-this-song, movie-this, do-what-it-says");
+        console.log("Please enter a command: \n node liri.js concert-this, \n node liri.js spotify-this-song, \n node liri.js movie-this, \n node liri.js do-what-it-says");
     }
 
 function concertThis(artistName) {
 
-        const URL = `https://rest.bandsintown.com/artists/${artistName}/events?app_id=codingbootcamp`;
+        let URL = `https://rest.bandsintown.com/artists/${artistName}/events?app_id=codingbootcamp`;
 
         axios.get(URL).then((response) => {
 
-            const jsonData = response.data;
+            let jsonData = response.data;
 
-            const showData = [
-                "*****************************************************************************",
+            let showData = [
+               
                 "Venue Name: " + jsonData[0].venue.name,
                 "Venue Location: " + jsonData[0].venue.city + ", " + jsonData[0].venue.country,
                 "Date and Time: " + moment(jsonData[0].datetime).format("LLL"),
-                "*****************************************************************************"
+                
             ].join("\n\n");
             
             return showData;
 
-    }).then(data=>{
+    }).then(data => {
         return storeAndDisplayData(data);
-    }).catch(err =>{
+    }).catch(err => {
         console.log(err);
      });
 }
@@ -79,67 +80,34 @@ function spotifyThis(song) {
 
             let jsonData = response.tracks.items;
 
-            let artist = jsonData[0].artist[0].name;
-            let track = jsonData[0].name;
-            let previewURL = jsonData[0].preview_url;
-            let album = jsonData[0].album.name;
+            let showData = [
 
-            if (response.tracks.total === 0 ) {
-                defaultSpotifySong()
+                "Artist(s): " + jsonData[0].artists[0].name,
+                "Song: " + jsonData[0].name,
+                "Album: " + jsonData[0].album.name,
+                "Preview Link: " + jsonData[0].preview_url,
 
-            } else {
-                console.log(`
-                ****************************
-                Artist: ${artist}
-                Track: ${track}
-                Preview URL: ${previewURL}
-                Album: ${album}
-                *****************************
-                `)
-            }
-        }).catch(err =>{
+            ].join("\n\n");
+
+            return showData;
+
+        }).then(data => {
+            return storeAndDisplayData(data);
+            
+        }).catch(err => {
             console.log(err);
-            console.log("No results found! Showing results for 'The Sign' by Ace of Base!");
+
         });
 }
 
-function defaultSpotifySong() {
-    spotify.search({ type: 'track', query: 'The Sign'})
-    .then((response) => {
-
-        let jsonData = response.tracks.items;
-
-        let artist = jsonData[0].artist[0].name;
-        let track = jsonData[0].name;
-        let previewURL = jsonData[0].preview_url;
-        let album = jsonData[0].album.name;
-
-        for (var i = 0; i < jsonData.length; i++) {
-            if (jsonData[i].artist[0].name === "Ace of Base") {
-                console.log(`
-                ******************************
-                Artist: ${artist}
-                Track: ${track}
-                Preview URL: ${previewURL}
-                Album: ${album}
-                ******************************
-                `)
-            }
-        }
-    }) .catch(err =>{
-        console.log(err);
-        console.log("No results found!");
-    });
-}
-
 function movieThis (movieTitle) {
-        const URL = `http://www.omdbapi.com/?t="${movieTitle}"&y=&plot=short&apikey=trilogy`;
+        let URL = `http://www.omdbapi.com/?t="${movieTitle}"&y=&plot=short&apikey=trilogy`;
         
         axios.get(URL).then((response) => {
-            const jsonData = response.data;
+            let jsonData = response.data;
 
-            const showData = [
-                "*****************************************************************************",
+            let showData = [
+               
                 "Title: " + jsonData.Title,
                 "Year: " + jsonData.Year,
                 "IMDb Rating: " + jsonData.imdbRating,
@@ -147,12 +115,12 @@ function movieThis (movieTitle) {
                 "Language: " + jsonData.Language,
                 "Plot: " + jsonData.Plot,
                 "Cast: " + jsonData.Actors,
-                "*****************************************************************************"
+               
             ].join("\n\n");
 
             return showData;
 
-        }).then(data=>{
+        }).then(data => {
             return storeAndDisplayData(data);
         }).catch(err =>{
             console.log(err);
